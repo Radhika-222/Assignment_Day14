19aed2fdb8ae1db85920103a25fa6ddc8a173aa8
Radhika-222 <149990790+Radhika-222@users.noreply.github.com>
2023-11-23 17:10:02 +0530

    Add files via upload

---
+++ ShapeAreaLib/ShapeAreaLib/Triangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeAreaLib
+{
+    public class Triangle
+    {
+        public double Area(double @base, double height)
+        {
+            double area = 0.5* @base * height;
+            return area;
+        }
+    }
+}
